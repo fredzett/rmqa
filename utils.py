219b2d593f0d83e966b2476864fbba23d05fb3e0
felix <felix@felixs-mbp.lan>
2020-09-17 10:11:56 +0200

    Descriptive statistics

--- utils.py
+++ utils.py
@@ -1,19 +1,5 @@
 import numpy as np
 
-def choice(data,n):
-    return np.random.choice(data,size=n)
-
-def normal(loc,scale,size):
-    return np.random.normal(loc,scale,size)
-
-def where(cond,conseq,alt):
-    n_x = sum(cond)
-    n_y = sum(~cond)
-    ret = np.zeros(n_x+n_y)
-    loc_x,scale_x = conseq
-    loc_y,scale_y = alt
-    x = normal(loc_x,scale_x,n_x)
-    y = normal(loc_y,scale_y,n_y)
-    ret[cond] = x
-    ret[~cond] = y
-    return ret
+def load_sales():
+    'Loads toy sales data'
+    return [2020,2075, 2125, 2040, 1980, 1955, 2050, 2165, 2070, 2260, 2060, 2040]
