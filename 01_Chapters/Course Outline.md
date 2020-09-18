431f91f86394a513ec63795df72cdfd36cb5de49
felix <felix@felixs-mbp.lan>
2020-09-18 10:40:24 +0200

    Test

---
+++ 01_Chapters/Course Outline.md
@@ -0,0 +1,5 @@
+# Course outline
+
+## Session 1:
+
+- Introduction to Jupyter Notebook: [link](../../Introduction_Jupyter_Notebooks.ipynb)
--- utils.py
+++ utils.py
@@ -1,5 +1,38 @@
 import numpy as np
+import pandas as pd
+from pathlib import Path
 
-def load_sales():
-    'Loads toy sales data'
-    return [2020,2075, 2125, 2040, 1980, 1955, 2050, 2165, 2070, 2260, 2060, 2040]
+def random_normal_clip(loc,std,size, low,high):
+    '''
+    Draw random samples from a (clipped) normal (Gaussian) distribution. 
+    
+    - low = minimal value for draw 
+    - high = maximum value for draw
+    
+    (if low = -np.inf and high=np.inf normal distribution is returned)
+    Draw is repeated until sample size is reached (with low/high condition)
+    
+    '''
+    data = []
+    while len(data) != size:
+        v = np.random.normal(loc,std)
+        if (v > low) & (v < high): data.append(v)
+    return data
+
+
+class Datasets():
+    'Helper class to load data sets'
+
+    ## Loading functions
+    @classmethod
+    def dax_monthly(self, output="list"):
+        print(Path.cwd())
+        path = "../../02_Datasets/dax.txt"
+        df = (pd.read_csv(path, usecols=["Adj Close"])
+             .rename({"Adj Close": "Price"},axis=1)
+             )
+        return df
+
+
+data = Datasets.dax_monthly()
+print(data)
